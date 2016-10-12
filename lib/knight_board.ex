bb8610b564fa3f44574807a664afe6a7de248c85
peteygao <me@petergao.com>
2016-10-11 19:03:04 -0700

    Implement board size and level selection.

--- lib/knight_board.ex
+++ lib/knight_board.ex
@@ -1,24 +1,40 @@
 defmodule KnightBoard do
   def main(args) do
-    {options, _, _} = OptionParser.parse(args, aliases: [h: :height, w: :width])
+    {options, _, _} =
+      OptionParser.parse(args,
+        aliases: [
+          h: :height,
+          w: :width,
+          l: :level,
+        ]
+      )
+
     case options do
       [width: width, height: height] ->
         initialize_board width, height
       [level: level] ->
         initialize_board level
       _ ->
-        IO.puts "No width or height provided, and no level was selected."
+        IO.puts "No --width or --height provided, and no --level was selected."
     end
   end
 
   defp initialize_board width, height do
-    IO.puts "Game board initialized."
-    IO.puts "width:\t#{width}"
-    IO.puts "height:\t#{height}"
+    init_message = """
+    Game board initialized.
+    width:  #{width}
+    height: #{height}
+    """
+
+    IO.puts init_message
   end
 
   defp initialize_board level do
-    IO.puts "Game board initialized."
-    IO.puts "Level:\t#{level}"
+    init_message = """
+    Game board initialized.
+    Level: #{level}
+    """
+
+    IO.puts init_message
   end
 end
